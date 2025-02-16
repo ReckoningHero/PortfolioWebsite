5f903648a253936007cf5be10550f8b0ddbb97c0
Joshua <joshua.nguyen@infovision.com>
2025-02-16 11:34:00 -0600

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -8,7 +8,7 @@
             muted
             playsinline
         >
-            <source src="https://hugo.fyi/res/video/hero1/hero1.mp4" type="video/mp4" />
+            <source src="https://ia601808.us.archive.org/24/items/unreal-engine-character-ui/%5BTutsNode.com%5D%20-%20Unreal%20Engine%20-%20Character%20UI/1.%20Project%20Setup/1.%20Project%20Primer.mp4" type="video/mp4" />
             Your browser does not support the video tag.
         </video>
 
@@ -21,10 +21,10 @@
         <!-- Content -->
         <div class="relative container mx-auto lg:px-[5em] px-[9em] text-white z-10 flex flex-col items-start justify-center h-full">
             <h3 class="uppercase lg:text-[5.1875em] text-[40px] font-atyp-display font-semibold lg:w-1/2 leading-[1.3em]">
-                Lorem ipsum dolor <span class="text-[#CCF303]">sit amet</span> adipiscing elit
+                Welcome to my realm! <span class="text-[#CCF303]"> Gameplay Engineer </span> loving to fix tech problems. 
             </h3>
             <p class="text-[#6B767F] lg:text-[1.375em] text-[16px] font-atyp-display font-medium lg:w-1/2 mt-[15px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in aliquet lacus, porttitor mollis neque donec gravida ultrices.
+                Code and .
             </p>
             <div class="flex gap-[21px] mt-[38px]">
                 <a href="/#contact" class="bg-[#CCF303] px-[23px] py-[15px] uppercase lg:text-[1.375em] text-[16px] text-black font-atyp-display font-medium shadow-[0_0_30px_#cbf30355] hover:shadow-[0_0_40px_#cbf30355] hover:translate-y-[-5px] transition-all">Let's have a chat</a>
