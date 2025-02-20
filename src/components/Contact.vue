6bf57f6695dfd8c353436f8a911d0a5f6caf808d
TwinB0rn <twinb0rn@protonmail.com>
2025-02-20 08:54:34 -0600

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <div id="contact" class="container mx-auto lg:px-[5em] px-[9em] mt-[59px]">
         <div class="bg-[#CCF303] text-[#030A10] lg:py-[93px] py-[43px] lg:px-[113px] px-[20px] flex flex-col items-center gap-[28px]">
             <h3 class="uppercase font-atyp-display font-semibold lg:text-[5.125em] text-[40px] text-center">Contact me</h3>
-            <p class="font-atyp-display font-medium text-[22px] text-center">Feel free to send me a message here: twinb0rn@protonmail.com, I'll try to respond as quick as I can! I'm also on this social media thing, if you need me there:</p>
+            <p class="font-atyp-display font-medium text-[22px] text-center">Feel free to send me a message here: joshua@twinb0rnsoft.com, I'll try to respond as quick as I can! I'm also on this social media thing, if you need me there:</p>
         </div>
     </div>
 </template>
